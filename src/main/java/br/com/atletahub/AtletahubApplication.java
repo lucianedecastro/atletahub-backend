36cb1021cad0476dd6877898806aaec02cf36861
Luciane <luciane.castro@gmail.com>
2025-05-25 13:25:51 -0300

    Ajustado application.properties para deploy no Railway e removido debug log

--- src/main/java/br/com/atletahub/AtletahubApplication.java
+++ src/main/java/br/com/atletahub/AtletahubApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AtletahubApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AtletahubApplication.class, args);
     }
