e32f931fa69e92743e692369d5c329a597a732ea
Luciane <luciane.castro@gmail.com>
2025-12-15 21:42:09 -0300

    ajustes aws

--- src/main/java/br/com/atletahub/atletahub_backend/traducao/config/AwsTranslateConfig.java
+++ src/main/java/br/com/atletahub/atletahub_backend/traducao/config/AwsTranslateConfig.java
@@ -1,24 +1,25 @@
 package br.com.atletahub.atletahub_backend.traducao.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.translate.TranslateClient;
 
-/**
- * Configuração do cliente AWS Translate.
- *
- * Usa:
- * - Variáveis de ambiente em local
- * - IAM Role automaticamente em cloud (Render / EC2 / ECS)
- */
 @Configuration
 public class AwsTranslateConfig {
+
+    @Value("${aws.region:us-east-1}")
+    private String awsRegion;
 
     @Bean
     public TranslateClient translateClient() {
         return TranslateClient.builder()
-                .region(Region.US_EAST_1) // ajuste se necessário
+                .region(Region.of(awsRegion))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .httpClient(UrlConnectionHttpClient.create())  // Mudança aqui
                 .build();
     }
 }
